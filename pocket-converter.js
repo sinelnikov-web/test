624181a0cace5abf677d7b0f923ededfe1ad845d
Maxim Sinelnikov <67955139+sinelnikov-web@users.noreply.github.com>
2021-08-12 18:33:32 +0600

    Add files via upload

--- pocket-converter.js
+++ pocket-converter.js
@@ -6,20 +6,38 @@
     let min_height = 100;
 
     function pocketConverter(msg) {
-
+        let css = jQuery('<link>');
+        css.attr({
+            rel: 'stylesheet',
+            type: 'text/css',
+            href: 'https://sinelnikov-web.github.io/test/pocket-converter.css'
+        });
+        jQuery('head').append(css);
+        box_html = `<div id="pocket-converter">
+                        <a href="#"id="close">&times;</a>
+                        <h1>Pocket Converter</h1>
+                        <div class="converter">
+                        
+                        </div>
+                    </div>`;
+        jQuery('body').append(box_html);
+        jQuery('#bookmarklet#close').click(function () {
+            jQuery('#bookmarklet').remove();
+        });
     }
+
     // Проверка, подключена ли jQuery.
-    if(typeof window.jQuery !== 'undefined') {
+    if (typeof window.jQuery !== 'undefined') {
         pocketConverter();
     } else {
         let conflict = typeof window.$ !== 'undefined';
         let script = document.createElement('script');
-        script.src = '//ajax.googleapis.com/ajax/libs/jquery/' +    jquery_version + '/jquery.min.js';
+        script.src = '//ajax.googleapis.com/ajax/libs/jquery/' + jquery_version + '/jquery.min.js';
         document.head.appendChild(script);
         let attempts = 15;
-        (function(){
-            if(typeof window.jQuery == 'undefined') {
-                if(--attempts > 0) {
+        (function () {
+            if (typeof window.jQuery == 'undefined') {
+                if (--attempts > 0) {
                     window.setTimeout(arguments.callee, 250)
                 } else {
                     alert('An error occurred while loading jQuery')
